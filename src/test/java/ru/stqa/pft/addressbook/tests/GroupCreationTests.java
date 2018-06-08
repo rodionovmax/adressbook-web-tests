887ba406bbc2dc7377f0a601c04767df81d593a8
rodionovmax90test@gmail.com <rodionovmax90test@gmail.com>
2018-06-07 20:11:11 -0400

    After lesson 4_3
    Added verifications for groups when you create, delete and modify groups test counts their quantity and gives error if their quantity don't match

--- src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -8,8 +9,12 @@ public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() {
+
         app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount(); //method initialization
         app.getGroupHelper().createGroup(new GroupData("test created for demo", "test promo HEADER", null));
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before + 1);
         //app.getGroupHelper().initGroupCreation();
         //app.getGroupHelper().fillGroupForm(new GroupData("test created for demo", "test promo HEADER", null));
         //app.getGroupHelper().submitGroupCreation();
--- src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
@@ -7,11 +8,13 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.tests.TestBase;
 
-public class GroupEditing extends TestBase {
+public class GroupModificationTests extends TestBase {
 
     @Test
     public void GroupEditing() {
+
         app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount();//initialize before modification verification of groups quantity
         if (! app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test1", "test HEADER", "test FOOTER"));
         }
@@ -20,7 +23,8 @@ public class GroupEditing extends TestBase {
         app.getGroupHelper().changeGroupForm();
         app.getGroupHelper().editGroupCreation();
         app.getGroupHelper().returnToGroupPage();
-
+        int after = app.getGroupHelper().getGroupCount(); //
+        Assert.assertEquals(after, before);
     }
 
 }
